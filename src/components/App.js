e16f93dc39a80c0db785a1a4a4605343a8a47d03
Prabesh <prabesh.shah31@gmail.com>
2018-10-06 22:27:35 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Create React App with CRA-CLI</h1>
+      </div>
+    );
+  }
+}
